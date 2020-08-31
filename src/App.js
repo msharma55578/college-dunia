4f1d0fce88f07bddfdb9c56b93d10359f1b7cbad
msharma55578 <m.sharma55578@gmail.com>
2020-08-31 11:07:12 +0530

    first final commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import MainPage from "./components/MainPage"
 import Trial from "./components/trial"
-
+import Trial_hai from "./components/trial_hai"
 function App() {
   return (
     <div className="App">
       {/* <MainPage/> */}
       <Trial/>
+      {/* <Trial_hai/> */}
     </div>
   );
 }
